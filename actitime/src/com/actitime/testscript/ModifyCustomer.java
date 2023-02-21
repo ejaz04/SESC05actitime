07aa750505f066d106aa795975a8660c83676109
Ejaz <Ejaz@DESKTOP-4357SKI>
2023-02-21 22:17:20 +0530

    Modify customer

---
+++ actitime/src/com/actitime/testscript/ModifyCustomer.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ModifyCustomer {
+   
+	@Test
+	public void modifyCustomer() {
+		Reporter.log("hi");
+	}
+	
+}
